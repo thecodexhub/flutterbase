ff00a25b6051c4344c84951ee1ab36f5c236c499
Sandip Pramanik <56641059+cdx-studio@users.noreply.github.com>
2020-08-16 19:49:01 +0530

    Add files via upload

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,13 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterbase/app/services/auth.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({@required this.onSignOut});
+  HomePage({@required this.auth, @required this.onSignOut});
   final VoidCallback onSignOut;
+  final AuthBase auth;
 
   Future<void> _signOut() async {
     try {
-      await FirebaseAuth.instance.signOut();
+      await auth.signOut();
       onSignOut();
     } catch (e) {
       print(e.toString());
