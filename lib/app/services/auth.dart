2035e5af6e143294d0aa3a91269d44572b62ee80
Sandip Pramanik <56641059+cdx-studio@users.noreply.github.com>
2020-08-23 21:12:31 +0530

    Add files via upload

--- lib/app/services/auth.dart
+++ lib/app/services/auth.dart
@@ -7,6 +7,7 @@ class User {
 }
 
 abstract class AuthBase {
+  Stream<User> get onAuthStateChanged;
   Future<User> currentUser();
   Future<User> signInAnonymously();
   Future<void> signOut();
@@ -20,6 +21,11 @@ class Auth implements AuthBase {
       return null;
     }
     return User(uid: user.uid);
+  }
+
+  @override
+  Stream<User> get onAuthStateChanged {
+    return _firebaseAuth.onAuthStateChanged.map(_userFromFirebase);
   }
 
   @override
